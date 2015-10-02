5e48a4c5be2126cd6a7f6c436c3776274c05a1e7
Poorva <poorvarane99@gmail.com>
2015-10-03 02:37:57 +0530

    Added .php file

--- homepage.php
+++ homepage.php
@@ -114,7 +114,7 @@
                         <li class="dropdown user user-menu">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <i class="glyphicon glyphicon-user"></i>
-                                <span>Jane Doe <i class="caret"></i></span>
+                                <span><?php echo $_POST["email"]; ?> <i class="caret"></i></span>
                             </a>
                             <ul class="dropdown-menu">
                                 <!-- User image -->
