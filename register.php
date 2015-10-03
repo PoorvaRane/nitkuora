d3e0832f91b019f53432a6eb62c9b11f625dbe06
Poorva <poorvarane99@gmail.com>
2015-10-03 23:53:16 +0530

    Basic php file made

---
+++ register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "password";
+        $dbname = "nitkuora";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT topic_name FROM topic";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo $row["topic_name"];
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+        ?> 
+    </body>
+</html>
