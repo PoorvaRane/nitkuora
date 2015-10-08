797dbcf36b958dc06f476f08cce59735ea79e9c6
Poorva <poorvarane99@gmail.com>
2015-10-08 23:07:28 +0530

    Updates topic-question table

--- post-question.php
+++ post-question.php
@@ -29,29 +29,40 @@
 
 			$sql = "INSERT INTO question (question_name, q_user_id, no_topic) VALUES ('$question', '$user','$topic_length')";
 
-			$sql3 = "SELECT question_id FROM question WHERE question_name = '$question' AND q_user_id to be continued ";
+			$sql3 = "SELECT question_id FROM question WHERE question_name = '$question' ";
 
-			foreach ($topic as $topicName) {
-				$sql1 = "SELECT topic_id FROM topic WHERE topic_name = '$topicName'";
-				$result1 = $conn->query($sql1);
-				array_push($topic_id_list, $result1->fetch_assoc());
+			$result3 = $conn->query($sql3);
+
+			if ($result3->num_rows > 0) {
+				echo "This question already exists.";
+			}
+			else {
+				$result = $conn->query($sql);
+				$result3 = $conn->query($sql3);				
+				$temp1 =  $result3->fetch_assoc();
+				$question_id = $temp1["question_id"];
+				
+				foreach ($topic as $topicName) {
+					$sql1 = "SELECT topic_id FROM topic WHERE topic_name = '$topicName'";
+					$result1 = $conn->query($sql1);
+					$temp = $result1->fetch_assoc();
+					array_push($topic_id_list, $temp["topic_id"]);
+				}
+
+				for($i=0;$i<$topic_length;$i++){
+
+					$sql2 = "INSERT INTO topic_question (topic_id, question_id) VALUES ('$topic_id_list[$i]', '$question_id' ) "; 
+
+					$result2 = $conn->query($sql2);
+
+				}
+
+				if($result === TRUE and $result2 === TRUE){
+					echo "YOUR QUESTION WAS SUCCESSFULLY POSTED. <br>";
+				}
 			}
 
-			for($i=0;$i<$topic_length;$i++){
-
-				$sql2 = "INSERT INTO topic_question (topic_id, question_id) VALUES ('$topic_id_list[$i]', '' ) "; 
-
-			}
-
-			
-
-			$result = $conn->query($sql);
-
-			if($result === TRUE){
-				echo "YOUR QUESTION WAS SUCCESSFULLY POSTED. <br>";
-			}
 		}
-		
 
 		$conn->close();
 		?>
