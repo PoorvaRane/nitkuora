10e82b90987b8a006d4b142ea2f5cc6a2cd8122a
Poorva <poorvarane99@gmail.com>
2015-10-10 21:26:27 +0530

    test commit

--- index.php
+++ index.php
@@ -92,7 +92,7 @@
                         <!-- search form -->
 		                    <form action="#" method="get" class="sidebar-form">
 		                        <div class="input-group">
-		                            <input type="text" name="q" class="form-control" placeholder="Search..."/>
+		                            <input type="text" name="q" class="form-control" placeholder="Search"/>
 		                            <span class="input-group-btn">
 		                                <button type='submit' name='seach' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
 		                            </span>
